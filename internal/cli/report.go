78b7d88ba2281417b4f5cda75eb2f4b73bd138b8
Jhionan <jhionan@gmail.com>
2026-05-20 20:36:54 +0200

    feat(cli): rules list/delete and report with date flags

--- internal/cli/report.go
+++ internal/cli/report.go
@@ -1,11 +1,64 @@
 package cli
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"sort"
+	"time"
+
+	"github.com/rian/antitimely/internal/rpcapi"
 )
 
 func cmdReport(args []string) int {
-	fmt.Fprintln(os.Stderr, "report: not implemented yet")
-	return 1
+	fs := flag.NewFlagSet("report", flag.ExitOnError)
+	fromStr := fs.String("from", "", "Inclusive start date YYYY-MM-DD (default today)")
+	toStr := fs.String("to", "", "Exclusive end date YYYY-MM-DD (default tomorrow)")
+	fs.Parse(args)
+
+	now := time.Now()
+	start := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+	end := start.Add(24 * time.Hour)
+	if *fromStr != "" {
+		t, err := time.ParseInLocation("2006-01-02", *fromStr, now.Location())
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return 64
+		}
+		start = t
+	}
+	if *toStr != "" {
+		t, err := time.ParseInLocation("2006-01-02", *toStr, now.Location())
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return 64
+		}
+		end = t
+	}
+
+	client, code := dialOrExit()
+	if client == nil {
+		return code
+	}
+	defer client.Close()
+	var reply rpcapi.ReportReply
+	if err := client.Call(rpcapi.ServiceName+".Report",
+		rpcapi.ReportArgs{FromUnix: start.Unix(), ToUnix: end.Unix()}, &reply); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	fmt.Printf("Report: %s → %s\n", start.Format("2006-01-02"), end.Format("2006-01-02"))
+	names := make([]string, 0, len(reply.Totals))
+	for k := range reply.Totals {
+		names = append(names, k)
+	}
+	sort.Strings(names)
+	for _, n := range names {
+		fmt.Printf("  %-30s %s\n", n, time.Duration(reply.Totals[n])*time.Second)
+	}
+	if reply.Unassigned > 0 {
+		fmt.Printf("  %-30s %s\n", "(unassigned)", time.Duration(reply.Unassigned)*time.Second)
+	}
+	return 0
 }
--- internal/cli/rules.go
+++ internal/cli/rules.go
@@ -1,11 +1,78 @@
 package cli
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"strconv"
+
+	"github.com/rian/antitimely/internal/rpcapi"
 )
 
 func cmdRules(args []string) int {
-	fmt.Fprintln(os.Stderr, "rules: not implemented yet")
-	return 1
+	if len(args) == 0 {
+		fmt.Fprintln(os.Stderr, "usage: antitimely rules <list|delete> ...")
+		return 64
+	}
+	switch args[0] {
+	case "list":
+		return rulesList()
+	case "delete":
+		return rulesDelete(args[1:])
+	default:
+		fmt.Fprintf(os.Stderr, "unknown subcommand: rules %s\n", args[0])
+		return 64
+	}
+}
+
+func rulesList() int {
+	client, code := dialOrExit()
+	if client == nil {
+		return code
+	}
+	defer client.Close()
+	var reply rpcapi.RulesListReply
+	if err := client.Call(rpcapi.ServiceName+".RulesList", rpcapi.RulesListArgs{}, &reply); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+	if len(reply.Items) == 0 {
+		fmt.Println("(no rules)")
+		return 0
+	}
+	fmt.Printf("%-4s %-3s %-15s %-30s %-30s %-15s %s\n",
+		"ID", "PRI", "PROJECT", "BUNDLE", "TITLE", "BINARY", "CWD-PREFIX")
+	for _, r := range reply.Items {
+		fmt.Printf("%-4d %-3d %-15s %-30s %-30s %-15s %s\n",
+			r.ID, r.Priority, r.ProjectName,
+			r.MatchBundleID, r.MatchTitleSubstr,
+			r.MatchBinaryName, r.MatchCWDPrefix)
+	}
+	return 0
+}
+
+func rulesDelete(args []string) int {
+	fs := flag.NewFlagSet("rules delete", flag.ExitOnError)
+	fs.Parse(args)
+	if fs.NArg() != 1 {
+		fmt.Fprintln(os.Stderr, "usage: antitimely rules delete <id>")
+		return 64
+	}
+	id, err := strconv.ParseInt(fs.Arg(0), 10, 64)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 64
+	}
+	client, code := dialOrExit()
+	if client == nil {
+		return code
+	}
+	defer client.Close()
+	if err := client.Call(rpcapi.ServiceName+".RuleDelete",
+		rpcapi.RuleDeleteArgs{ID: id}, &rpcapi.RuleDeleteReply{}); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+	fmt.Printf("Deleted rule %d\n", id)
+	return 0
 }
