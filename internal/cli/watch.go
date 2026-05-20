7aaa99734e0a13725d885bbc922e25acc410c924
Jhionan <jhionan@gmail.com>
2026-05-20 20:36:15 +0200

    feat(cli): watch add/list/remove

--- internal/cli/watch.go
+++ internal/cli/watch.go
@@ -2,10 +2,116 @@ package cli
 
 import (
 	"fmt"
+	"net/rpc"
 	"os"
+
+	"github.com/rian/antitimely/internal/rpcapi"
 )
 
 func cmdWatch(args []string) int {
-	fmt.Fprintln(os.Stderr, "watch: not implemented yet")
-	return 1
+	if len(args) == 0 {
+		fmt.Fprintln(os.Stderr, "usage: antitimely watch <add|list|remove> ...")
+		return 64
+	}
+	switch args[0] {
+	case "list":
+		return watchList()
+	case "add":
+		return watchAdd(args[1:])
+	case "remove":
+		return watchRemove(args[1:])
+	default:
+		fmt.Fprintf(os.Stderr, "unknown subcommand: watch %s\n", args[0])
+		return 64
+	}
+}
+
+func watchList() int {
+	client, code := dialOrExit()
+	if client == nil {
+		return code
+	}
+	defer client.Close()
+
+	var reply rpcapi.WatchListReply
+	if err := client.Call(rpcapi.ServiceName+".WatchList", rpcapi.WatchListArgs{}, &reply); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+	if len(reply.Items) == 0 {
+		fmt.Println("(no programs watched)")
+		return 0
+	}
+	for _, item := range reply.Items {
+		fmt.Printf("%-8s %s\n", item.Kind, item.Identifier)
+	}
+	return 0
+}
+
+func watchAdd(args []string) int {
+	if len(args) != 2 {
+		fmt.Fprintln(os.Stderr, "usage: antitimely watch add <app|binary> <identifier>")
+		return 64
+	}
+	kind := args[0]
+	switch kind {
+	case "app", "bundle":
+		kind = "bundle"
+	case "binary":
+		// canonical
+	default:
+		fmt.Fprintln(os.Stderr, "kind must be 'app' (bundle id) or 'binary' (process name)")
+		return 64
+	}
+	client, code := dialOrExit()
+	if client == nil {
+		return code
+	}
+	defer client.Close()
+
+	if err := client.Call(rpcapi.ServiceName+".WatchAdd",
+		rpcapi.WatchAddArgs{Kind: kind, Identifier: args[1]},
+		&rpcapi.WatchAddReply{}); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+	fmt.Printf("Added: %s %s\n", kind, args[1])
+	return 0
+}
+
+func watchRemove(args []string) int {
+	if len(args) != 1 {
+		fmt.Fprintln(os.Stderr, "usage: antitimely watch remove <identifier>")
+		return 64
+	}
+	client, code := dialOrExit()
+	if client == nil {
+		return code
+	}
+	defer client.Close()
+
+	for _, kind := range []string{"bundle", "binary"} {
+		_ = client.Call(rpcapi.ServiceName+".WatchRemove",
+			rpcapi.WatchRemoveArgs{Kind: kind, Identifier: args[0]},
+			&rpcapi.WatchRemoveReply{})
+	}
+	fmt.Printf("Removed: %s\n", args[0])
+	return 0
+}
+
+// dialOrExit dials the daemon socket. Returns (nil, exitCode) on failure.
+// Lives in watch.go since it's the first command that needs it; later
+// subcommands import it from the same package.
+func dialOrExit() (*rpc.Client, int) {
+	sockPath, err := DefaultSocketPath()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return nil, 1
+	}
+	c, err := Dial(sockPath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return nil, 2
+	}
+	return c, 0
 }
